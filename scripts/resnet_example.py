99f7c3054042233a4d9c9e8cfaac5049b5f3f385
Mohammed Fellaji <fellajimohammed@gmail.com>
2024-09-17 10:59:14 +0200

    example: resnet18 - prune and quantize Results: - Weight Pruning: nb_zeros/all_params=30.23% * Quantization gain: size_source/size_dest_quant=398.02% (size_source=4.675e+04MB - size_dest_quant=1.175e+04MB) * Pruning and Quantization gain: size_source/size_dest_pruned_quant=398.02% (size_source=4.675e+04MB - size_dest_pruned_quant=1.175e+04MB)

--- scripts/resnet_example.py
+++ scripts/resnet_example.py
@@ -1,4 +1,5 @@
 import os
+import onnx
 import torch
 from torchvision import models
 from pathlib import Path
@@ -10,7 +11,8 @@ from pyprune.onnx_module import ONNXModule
 if __name__ == "__main__":
     project_path = Path(__file__).parents[1].resolve().absolute()
     source = project_path / 'resnet.onnx'
-    dest = project_path / 'resnet_quant.onnx'
+    dest_quant = project_path / 'resnet_quant.onnx'
+    dest_pruned_quant = project_path / 'resnet_pruned_quant.onnx'
 
     # model
     model = models.resnet18(weights='IMAGENET1K_V1')
@@ -20,14 +22,26 @@ if __name__ == "__main__":
     ONNXModule.export(model, dummy_input, source)
 
     # quantize ONNX model
-    onnx_quantize(source, dest)
+    onnx_quantize(source, dest_quant)
 
-    size_source = os.stat(source).st_size
-    size_dest = os.stat(dest).st_size
+    # get sizes
+    size_source = os.stat(source).st_size / 1e3
+    size_dest_quant = os.stat(dest_quant).st_size / 1e3
 
-    print(f"* Quantization gain: {size_source/size_dest=:.2%}",
-          f"({size_source=:.3e}B - {size_dest=:.3e}B)")
+    # pruning
+    onnx_module = ONNXModule(onnx_path=source)
+    onnx_module.prune(threshold=1e-2, verbose=True)
+    onnx.save(onnx_module.onnx_model, dest_pruned_quant)
+    onnx_quantize(dest_pruned_quant, dest_pruned_quant)
+    size_dest_pruned_quant = os.stat(dest_pruned_quant).st_size / 1e3
+
+    print(f"* Quantization gain: {size_source/size_dest_quant=:.2%}",
+          f"({size_source=:.3e}MB - {size_dest_quant=:.3e}MB)")
+    print("* Pruning and Quantization gain:",
+          f"{size_source/size_dest_pruned_quant=:.2%}",
+          f"({size_source=:.3e}MB - {size_dest_pruned_quant=:.3e}MB)")
 
     # cleaning
     source.unlink(missing_ok=True)
-    dest.unlink(missing_ok=True)
+    dest_quant.unlink(missing_ok=True)
+    dest_pruned_quant.unlink(missing_ok=True)
